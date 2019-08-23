5aff3f33c3376b536da842b6a7da3fb2e5098828
李腾 <iloveafterschollteatime@gmail.com>
2019-08-23 11:44:14 +0800

    抓取函数同步有问题,数据错位

--- JS/dataCatch/dataCatch.js
+++ JS/dataCatch/dataCatch.js
@@ -1,6 +1,11 @@
 var superagent = require("superagent");
+var cheerIo = require('cheerio');
 require('superagent-proxy')(superagent);
 var model = require('../../models/model');
+var url = "mongodb://localhost:27017/doubanDatabases"
+var mongoClient = require('mongodb').MongoClient;
+
+
 
 
 
@@ -47,46 +52,146 @@ function saveAllData() {
 }
 
 
-function saveCountryMovie(name_EN,name_CN)//抓取每个国家的电影,
- {
+function saveCountryMovie(name_EN, name_CN) //抓取每个国家的电影,
+{
     var i = 0;
     //var proxy = process.env.http_proxy || 'http://127.0.0.1:1080';
-    
+
 
     function catchData() {
         var JsonData;
         var movieOfAmericaUrl = encodeURI(`https://movie.douban.com/j/new_search_subjects?sort=U&range=0,10&tags=电影&start=${i*20}&countries=${name_CN}&year_range=2009,2019`)
-        
-            superagent.get(movieOfAmericaUrl).end(function (error, data) {
-                if (error) throw error;
-                JsonData = JSON.parse(data.text);
-                for (let j in JsonData.data) //JsonData为一个JSON对象,里面的data部分才是数组
-                {
-                    model.saveCountryData(JsonData.data[j],name_EN);
-                }
+
+        superagent.get(movieOfAmericaUrl).end(function (error, data) {
+            if (error) throw error;
+            JsonData = JSON.parse(data.text);
+            for (let j in JsonData.data) //JsonData为一个JSON对象,里面的data部分才是数组
+            {
+                model.saveCountryData(JsonData.data[j], name_EN);
+            }
 
 
-                console.log("现在已经抓取到" + i * 20 + "   " + "现在长度" + JsonData.data.length);
-                if (JsonData.data.length < 20) {
-                    return;
-                }
-                catchDataCircle();
-            });
-       
+            console.log("现在已经抓取到" + i * 20 + "   " + "现在长度" + JsonData.data.length);
+            if (JsonData.data.length < 20) {
+                return;
+            }
+            catchDataCircle();
+        });
+
 
 
 
 
     }
     catchData();
+
     function catchDataCircle() {
         i = i + 1;
         catchData();
     }
-    
+
 
 }
 
 //saveCountryMovie('china','中国大陆');
-saveAllData();//不分国家抓取
+//saveAllData();//不分国家抓取
+function addCountry() {
+    var dataIndex=0;
+    function getAllData(callback) {
+        mongoClient.connect(url, {
+            useNewUrlParser: true
+        }, function (error, db) {
+            if (error) throw err;
+            var dbTmp = db.db("doubanDatabases");
+            dbTmp.collection('main').find().toArray(function (err, result) {
 
+                var catchInterval=setInterval(
+                    function(){
+                        var countryName;
+                        var url = encodeURI(result[dataIndex].url);
+                        superagent.get(url).end(function (err, data) {
+                            if (err) throw err;
+    
+                            var $ = cheerIo.load(data.text);
+                            var infoString = $('#info').text();
+                            var countryIndex = infoString.indexOf("制片国家/地区");
+                            var languageIndex = infoString.indexOf("语言");
+                            countryName = infoString.substring(countryIndex+9, languageIndex).trim();
+                            callback(result[dataIndex],countryName);
+                            console.log("获取到了第"+dataIndex+"个数据");
+                            if(dataIndex>9978)
+                            {
+                                clearInterval(catchInterval);
+                            }
+                            dataIndex++;
+                            
+    
+                        });
+                    },1000
+                )
+               /* for (let item of result) {
+
+                    var countryName;
+                    var url = encodeURI(item.url);
+                    superagent.get(url).end(function (err, data) {
+                        if (err) throw err;
+
+                        var $ = cheerIo.load(data.text);
+                        var infoString = $('#info').text();
+                        var countryIndex = infoString.indexOf("制片国家/地区");
+                        var languageIndex = infoString.indexOf("语言");
+                        countryName = infoString.substring(countryIndex+9, languageIndex).trim();
+                        callback(item,countryName);
+
+
+                    });
+
+                    
+                }*/
+            })
+        })
+
+
+    }
+
+    function saveCountryData(object,countryName) {
+
+        var tmp = {
+            directors: object.directors,
+            rate: object.rate,
+            cover_x: object.cover_x,
+            star: object.star,
+            title: object.title,
+            url: object.url,
+            casts: object.casts,
+            cover: object.cover,
+            _id: object._id,
+            cover_y: object.cover_y,
+            country:countryName
+        }
+
+        mongoClient.connect(url, {
+            useNewUrlParser: true
+        }, function (error, db) {
+            if (error) {
+                console.log(链接数据库失败error)
+            }
+
+            var nowDb = db.db('doubanDatabases');
+            try {
+                nowDb.collection(countryName).insertOne(tmp, function (err, res) {
+                    db.close();
+                })
+            } catch (error) {
+                console.log(插入 + tmp + 数据失败 + err)
+                db.close();
+
+            }
+
+        })
+    }
+
+    getAllData(saveCountryData);
+}
+
+addCountry()
--- models/model.js
+++ models/model.js
@@ -1,7 +1,9 @@
 var url = "mongodb://localhost:27017/doubanDatabases"
 var mongoClient = require('mongodb').MongoClient;
+var superagent = require("superagent");
 
-//前面三个为获取数据的时候使用的函数
+
+//前面两个个为获取数据的时候使用的函数
 function saveAllData(object) {
 
     var tmp = {
@@ -80,60 +82,6 @@ function saveCountryData(object, collectionName) {
 
 }
 
-function addCountry() {
-    function getAllData(callback) {
-        mongoClient.connect(url, {
-            useNewUrlParser: true
-        }, function (error, db) {
-            if (err) throw err;
-            var dbTmp = db.db("doubanDatabases");
-            dbTmp.collection('main').find().toArray(function (err, result) {
-                for (let item of result) {
-                    callback(item);
-                }
-            })
-        })
-
-
-    }
-
-    function saveCountryData(object) {
-        var tmp = {
-            directors: object.directors,
-            rate: parseFloat(object.rate),
-            cover_x: parseInt(object.cover_x),
-            star: parseInt(object.star),
-            title: object.title,
-            url: object.url,
-            casts: object.casts,
-            cover: object.cover,
-            _id: parseInt(object.id),
-            cover_y: parseInt(object.cover_y)
-        }
-
-        mongoClient.connect(url, {
-            useNewUrlParser: true
-        }, function (error, db) {
-            if (error) {
-                console.log(链接数据库失败error)
-            }
-
-            var nowDb = db.db('doubanDatabases');
-            try {
-                nowDb.collection(collectionName).insertOne(tmp, function (err, res) {
-                    db.close();
-                })
-            } catch (error) {
-                console.log(插入 + tmp + 数据失败 + err)
-                db.close();
-
-            }
-
-        })
-    }
-
-
-}
 
 
 function findCountryDocument(countryName, callback) {
