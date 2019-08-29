295b74880f19de7c58523e3364ed4a2f1bf65f92
李腾 <iloveafterschollteatime@gmail.com>
2019-08-29 19:20:59 +0800

    解决跨域问题,添加注释

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ app.use("/plugin",express.static("plugin"));
 app.use("/JS",express.static("JS"));
 app.use("/CSS",express.static("CSS"))
 app.get("/",controller.mainPage);
+
+//解决跨域问题
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
